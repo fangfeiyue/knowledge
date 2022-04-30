5811c021bf8b78797857e564ad126b8cc65e5def
FangFeiyue <18766131540@163.com>
2022-04-30 19:02:35 +0800

    script setup初步使用

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,11 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <user :name="name" @change-name="changeName"/>
 </template>
-
-<script lang="ts">
-import { defineComponent } from 'vue'
-import HelloWorld from './components/HelloWorld.vue'
-
-export default defineComponent({
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-})
-</script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<script setup lang="ts">
+import { ref } from 'vue'
+import User from './components/User.vue'
+const name = ref('f')
+const changeName = (val:string) => {
+  name.value = val + 'y'
 }
-</style>
+</script>
---
+++ src/components/User.vue
@@ -0,0 +1,14 @@
+<template>
+  <div @click="onClick">{{ name }}</div>
+  <div></div>
+</template>
+<script setup lang="ts">
+import { defineProps, defineEmits } from 'vue'
+const props = defineProps({
+  name: String
+})
+const emit = defineEmits(['change-name'])
+const onClick = () => {
+  emit('change-name', props.name)
+}
+</script>
