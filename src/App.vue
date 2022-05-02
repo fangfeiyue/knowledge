b303d9e094e11d8b2c4fcd332adc20dd19c2ba0e
FangFeiyue <18766131540@163.com>
2022-05-02 16:19:38 +0800

    编写ColumnList

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,33 @@
 <template>
-  <User :user="user" @change-name="changeName"/>
-  <ColumnList/>
+  <ColumnList :list="list"/>
 </template>
 <script setup lang="ts">
-import { reactive } from 'vue'
-import User from './components/User.vue'
-import ColumnList from './components/ColumnList.vue'
-const user = reactive({ name: 'f' })
-const changeName = (val:string) => {
-  user.name = val + 'y'
-}
+import 'bootstrap/dist/css/bootstrap.min.css'
+import ColumnList, { ColumnProps } from './components/ColumnList.vue'
+const list: ColumnProps[] = [
+  {
+    id: 1,
+    title: '你好',
+    // avatar: 'https://www.baidu.com/img/PCfb_5bf082d29588c07f842ccde3f97243ea.png',
+    description: '你好你好'
+  },
+  {
+    id: 1,
+    title: '你好',
+    avatar: 'https://www.baidu.com/img/PCfb_5bf082d29588c07f842ccde3f97243ea.png',
+    description: '你好你好'
+  },
+  {
+    id: 1,
+    title: '你好',
+    avatar: 'https://www.baidu.com/img/PCfb_5bf082d29588c07f842ccde3f97243ea.png',
+    description: '你好你好'
+  },
+  {
+    id: 1,
+    title: '你好',
+    avatar: 'https://www.baidu.com/img/PCfb_5bf082d29588c07f842ccde3f97243ea.png',
+    description: '你好你好'
+  }
+]
 </script>
---
+++ src/components/ColumnList.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div v-for="item in columnList" :key="item.id" class="col-4 mb-4">
+        <div class="card h-100 shadow-sm" style="width: 18rem;">
+          <img :src="item.avatar" class="rounded-circle border border-light my-3" :alt="item.title">
+          <div class="card-body text-center">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text text-left">Some quick example text to build on the card title and make up the bulk of the card's
+              content.</p>
+            <a href="#" class="btn btn-outline-primary">Go somewhere</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script setup lang="ts">
+import { defineProps, PropType, computed } from 'vue'
+export interface ColumnProps {
+  id: number,
+  title: string,
+  avatar?: string,
+  description: string
+}
+const props = defineProps({
+  list: {
+    type: Array as PropType<ColumnProps[]>,
+    required: true
+  }
+})
+const columnList = computed(() => props.list.map(column => {
+  if (!column.avatar) column.avatar = 'https://lf3-cdn-tos.bytescm.com/obj/static/xitu_juejin_web/e08da34488b114bd4c665ba2fa520a31.svg'
+  return column
+}))
+</script>
