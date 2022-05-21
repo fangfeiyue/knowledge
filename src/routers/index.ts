6944ec831945df4b97258b9131a8e8867d9f2f9f
FangFeiyue <18766131540@163.com>
2022-05-21 11:28:16 +0800

    路由守卫

--- src/routers/index.ts
+++ src/routers/index.ts
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
 import Login from '../views/Login.vue'
 import ColumnDetail from '../views/ColumnDetail.vue'
+import store from '../store/index'
 
 const routerHistory = createWebHistory()
 const router = createRouter({
@@ -15,14 +16,29 @@ const router = createRouter({
     {
       path: '/login',
       name: 'login',
-      component: Login
+      component: Login,
+      meta: { isLoginPage: true }
     },
     {
       path: '/column/:id',
       name: 'column',
-      component: ColumnDetail
+      component: ColumnDetail,
+      meta: { isAuth: true }
     }
   ]
+})
+
+router.beforeEach((to, from, next) => {
+  const { meta: { isAuth, isLoginPage } } = to
+  const { user: { isLogin } } = store.state
+  if (isAuth && !isLogin) {
+    next('/login')
+    // next({ name: 'login' })
+  } else if (isLoginPage && isLogin) {
+    next('/')
+  } else {
+    next()
+  }
 })
 
 export default router
