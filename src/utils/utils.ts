48756f28a5a259247dcdfd392b47b4f0ce6285af
FangFeiyue <18766131540@163.com>
2022-05-25 16:14:22 +0800

    添加图片压缩函数

---
+++ src/utils/utils.ts
@@ -0,0 +1,10 @@
+import { IColumnProps, IImageProps, IUserProps } from '../store/index'
+
+export function generateFitUrl(data: IImageProps, width: number, height: number, format = ['m_pad']) {
+  if (data && data.url) {
+    const formatStr = format.reduce((prev, current) => {
+      return current + ',' + prev
+    }, '')
+    data.fitUrl = data.url + `?x-oss-process=image/resize,${formatStr}h_${height},w_${width}`
+  }
+}
