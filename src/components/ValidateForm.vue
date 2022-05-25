6b0cc7466a76d71384ce331aca1a09a8f59ab116
FangFeiyue <18766131540@163.com>
2022-05-25 16:10:32 +0800

    更新form组件

--- src/components/ValidateForm.vue
+++ src/components/ValidateForm.vue
@@ -1,8 +1,7 @@
 <template>
-  <form class="validate-form-container" @submit.prevent="submitForm">
-    <slot></slot>
-    <!-- <div class="submit-area" @click.prevent="submitForm"> -->
-    <div class="submit-area">
+  <form class="validate-form-container">
+    <slot name="default"></slot>
+    <div class="submit-area" @click.prevent="submitForm">
       <slot name="submit">
         <button type="submit" class="btn btn-primary">提交</button>
       </slot>
