80ca1f60cf24026fe62559bb5157219611e43cba
FangFeiyue <18766131540@163.com>
2022-05-25 16:15:50 +0800

    使用vuex访问接口

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,31 +1,90 @@
 import { createStore } from 'vuex'
-import { testData, testPosts, ColumnProps, PostProps } from '../mock/data'
-interface IUserProps {
+import { getColumns, getPosts, getColumn } from '../api/index'
+export interface IUserProps {
   isLogin: boolean,
   name?: string,
-  id?: number
+  id?: number,
+  columnId?: number
+}
+export interface IImageProps {
+  _id?: string,
+  url?: string,
+  createAt?: string,
+  fitUrl?: string;
+}
+export interface IColumnProps {
+  _id: string,
+  title: string,
+  avatar?: IImageProps,
+  description: string
+}
+export interface IPostsProps {
+  _id?: string;
+  title: string;
+  excerpt?: string;
+  content?: string;
+  image?: IImageProps;
+  createdAt?: string;
+  column: string;
+  author?: string | IUserProps;
+  isHTML?: boolean;
 }
 export interface IGlobalDataProps {
-  columns: ColumnProps[],
-  posts: PostProps[],
+  columns: IColumnProps[],
+  posts: IPostsProps[],
   user: IUserProps
 }
 const store = createStore<IGlobalDataProps>({
   state: {
-    posts: testPosts,
-    columns: testData,
+    posts: [],
+    columns: [],
     user: {
-      isLogin: false
+      name: 'f',
+      columnId: 1,
+      isLogin: true
     }
   },
   mutations: {
     login(state) {
       state.user = { ...state.user, isLogin: true, name: 'f' }
+    },
+    createPost(state, newPost) {
+      state.posts.push(newPost)
+    },
+    getColumns(state, list) {
+      state.columns = list
+    },
+    getColumn(state, data) {
+      state.columns = [data]
+      // state.columns = [data.data]
+    },
+    getPosts(state, data) {
+      state.posts = data
+      // state.posts = data.data.list
     }
   },
   getters: {
-    getColumnById: (state) => (id: number) => state.columns.find(item => item.id === id),
-    getPostByCid: (state) => (cid: number) => state.posts.filter(item => item.id === cid)
+    getColumnById: (state) => (id: string) => state.columns.find(item => item._id === id),
+    getPostByCid: (state) => (cid: string) => state.posts.filter(item => item.column === cid)
+  },
+  actions: {
+    fetchColumns({ commit }) {
+      getColumns().then(res => {
+        const { list } = res
+        commit('getColumns', list)
+      })
+    },
+    fetchColum({ commit }, cid) {
+      getColumn(cid).then(res => {
+        commit('getColumn', res)
+      })
+    },
+    fetchPosts({ commit }, cid) {
+      getPosts(cid).then(res => {
+        const list = res.list
+        commit('getPosts', list)
+      })
+    }
   }
 })
 export default store
