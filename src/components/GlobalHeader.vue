6d46bf01a1de9f98460cf2df0d541d6e1ed2711b
FangFeiyue <18766131540@163.com>
2022-05-25 16:10:00 +0800

    更新header组件

--- src/components/GlobalHeader.vue
+++ src/components/GlobalHeader.vue
@@ -1,18 +1,18 @@
 <template>
   <nav class="navbar navbar-dark bg-primary justify-content-between mb-4 px-4">
-    <a class="navbar-brand" href="#">Navbar</a>
+    <router-link class="navbar-brand" to="/">知识</router-link>
     <ul v-if="!user.isLogin" class="list-inline mb-0">
       <li class="list-inline-item"><router-link to="/login" class="btn btn-outline-light my-2">登陆</router-link></li>
       <li class="list-inline-item"><router-link to="/signup" class="btn btn-outline-light my-2">注册</router-link></li>
     </ul>
     <ul v-else class="list-inline mb-0">
       <li class="list-inline-item">
-        <DropDown :title="`你好 ${user.name}`">
-          <DropDownItem><router-link to='/create' class="dropdown-item">创建文章</router-link></DropDownItem>
-          <DropDownItem><router-link to='`/column/${user.id}`' class="dropdown-item">我的专栏</router-link></DropDownItem>
-          <DropDownItem disabled><a href="#" class="dropdown-item">编辑资料</a></DropDownItem>
-          <DropDownItem><a href="#" class="dropdown-item">退出登录</a></DropDownItem>
-        </DropDown>
+        <drop-down :title="`你好 ${user.name}`">
+          <dropdown-item><router-link to="/create" class="dropdown-item">新建文章</router-link></dropdown-item>
+          <dropdown-item><router-link :to="`/column/${user.id}`" class="dropdown-item">我的专栏</router-link></dropdown-item>
+          <dropdown-item disabled><a href="#" class="dropdown-item">编辑资料</a></dropdown-item>
+          <dropdown-item><a href="#" class="dropdown-item">退出登陆</a></dropdown-item>
+        </drop-down>
       </li>
     </ul>
   </nav>
