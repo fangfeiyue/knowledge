e18f205b251b4adc68b3b6bce84d34ea6c05c013
FangFeiyue <18766131540@163.com>
2022-05-30 17:27:52 +0800

    message组件添加destroy方法

--- src/components/createMessage.ts
+++ src/components/createMessage.ts
@@ -1,15 +1,13 @@
-import { createApp, h, render } from 'vue'
+import { h, render } from 'vue'
 // Module '"*.vue"' has no exported member 'MsgType'.
 // import Message, { MsgType } from './Message.vue'
 import Message from './Message.vue'
 
 export type MsgType = 'error' | 'success' | 'default'
-
-const createMessage = (msg:string, type: MsgType, timeout = 2000): void => {
-  // const instance = createApp(Message, {
-  //   msg,
-  //   type
-  // })
+interface Fn {
+  destroy:()=>void
+}
+const createMessage = (msg:string, type: MsgType, timeout?: number):Fn => {
   const instance = h(Message, {
     msg,
     type
@@ -17,13 +15,22 @@ const createMessage = (msg:string, type: MsgType, timeout = 2000): void => {
   const node = document.createElement('div')
   document.body.appendChild(node)
 
-  // instance.mount(node)
-  render(instance, node)
-  setTimeout(() => {
-    // instance.unmount()
+  const destroy = ():void => {
     render(null, node)
     document.body.removeChild(node)
-  }, timeout)
+  }
+
+  render(instance, node)
+
+  if (timeout) {
+    setTimeout(() => {
+      destroy()
+    }, timeout)
+  }
+
+  return {
+    destroy
+  }
 }
 
 export default createMessage
