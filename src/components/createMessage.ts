74d8fa239b287454a15c99de890cd3d28fadf3fb
FangFeiyue <18766131540@163.com>
2022-05-30 17:17:34 +0800

    用h函数改造message组件

--- src/components/createMessage.ts
+++ src/components/createMessage.ts
@@ -1,4 +1,4 @@
-import { createApp } from 'vue'
+import { createApp, h, render } from 'vue'
 // Module '"*.vue"' has no exported member 'MsgType'.
 // import Message, { MsgType } from './Message.vue'
 import Message from './Message.vue'
@@ -6,16 +6,22 @@ import Message from './Message.vue'
 export type MsgType = 'error' | 'success' | 'default'
 
 const createMessage = (msg:string, type: MsgType, timeout = 2000): void => {
-  const instance = createApp(Message, {
+  // const instance = createApp(Message, {
+  //   msg,
+  //   type
+  // })
+  const instance = h(Message, {
     msg,
     type
   })
   const node = document.createElement('div')
   document.body.appendChild(node)
 
-  instance.mount(node)
+  // instance.mount(node)
+  render(instance, node)
   setTimeout(() => {
-    instance.unmount()
+    // instance.unmount()
+    render(null, node)
     document.body.removeChild(node)
   }, timeout)
 }
