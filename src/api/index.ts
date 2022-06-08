dc8fdcd0f4367cd745a2a5991c9b0ce924ac77af
FangFeiyue <18766131540@163.com>
2022-06-08 16:40:22 +0800

    上传图片接口

--- src/api/index.ts
+++ src/api/index.ts
@@ -20,15 +20,37 @@ export function getColumn(params?: any) {
     data: null
   })
 }
-export function login(params?: any) {
+type Login = {
+  email: string,
+  password: string
+}
+export function login(params?: Login):Promise<any> {
   return ajax.post({
     url: '/user/login',
     data: params
   })
 }
-export function getUser(params?: any) {
+export function getUser(params?: void):Promise<any> {
   return ajax.get({
     url: '/user/current',
+    data: params
+  })
+}
+
+type RegisterType = {
+  'email': string,
+  'nickName': string,
+  'password': string
+}
+export function register(params?:RegisterType):Promise<any> {
+  return ajax.post({
+    url: '/users',
+    data: params
+  })
+}
+export function upload(params?: any):Promise<any> {
+  return ajax.post({
+    url: '/upload',
     data: params
   })
 }
