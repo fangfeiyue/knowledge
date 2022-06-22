c52a918764876373c11e74c91320525c15bc2c9a
FangFeiyue <18766131540@163.com>
2022-06-22 16:42:02 +0800

    新增UserProfile组件

---
+++ src/components/UserProfile.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="user-profile-component">
+    <div class="d-flex align-items-center">
+      <img :src="fitUrl" :alt="user.nickName" class="rounded-circle img-thumbnail">
+      <div class="detail ml-2">
+        <h6 class="d-block mb-0">{{user.nickName}}</h6>
+        <span class="text-truncate text-muted d-block">{{user.description}}</span>
+      </div>
+    </div>
+  </div>
+</template>
+<script setup lang="ts">
+import { defineProps, computed, PropType } from 'vue'
+import { addColumnAvatar } from '@/utils/utils'
+import { IUserProps } from '@/store/index'
+const props = defineProps({
+  user: {
+    type: Object as PropType<IUserProps>,
+    required: true
+  }
+})
+const fitUrl = computed(() => {
+  addColumnAvatar(props.user, 50, 50)
+  const { avatar } = props.user
+  return avatar && avatar.fitUrl
+})
+</script>
+<style scoped>
+.user-profile-component img {
+  width: 50px;
+  height: 50px;
+  /* border: 1px solid #ccc;
+  border-radius: 50px; */
+}
+</style>
