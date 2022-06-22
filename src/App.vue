e741fc76c36de1f05d4039783c3db41447d504cb
FangFeiyue <18766131540@163.com>
2022-06-22 16:40:37 +0800

    去掉图片上传测试代码

--- src/App.vue
+++ src/App.vue
@@ -1,44 +1,21 @@
 <template>
-  <div class="container">
+  <div class="container-fluid px-0 flex-shrink-0">
     <global-header :user="user" />
-    <uploader
-      action="/upload"
-      @file-uploaded="handleFileUploaded"
-      :beforeUpload="beforeUpload"
-      :uploaded="uploadedData"
-      class="d-flex align-items-center justify-content-center bg-light text-secondary w-100 my-4"
-    >
-      <template #uploaded="dataProps">
-        <img :src="dataProps.uploadedData && dataProps.uploadedData.url" style="width:500px;"/>
-      </template>
-    </uploader>
     <router-view/>
     <global-footer/>
   </div>
 </template>
 <script setup lang="ts">
-import { computed, watch, ref } from 'vue'
+import { computed, watch } from 'vue'
 import { useStore } from 'vuex'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import GlobalFooter from './components/GlobalFooter.vue'
 import GlobalHeader from './components/GlobalHeader.vue'
 import createMessage from '@/components/createMessage'
-import Uploader from './components/Uploader.vue'
-import { IImageProps } from '@/store/index'
 
 const store = useStore()
-const uploadedData = ref()
-let imageId = ''
 const user = computed(() => store.state.user)
 const error = computed(() => store.state.error)
-const beforeUpload = (file:File) => {
-  const isValide = file.type === 'image/png'
-  if (!isValide) createMessage('上传的图片只能是 JPG 格式', 'error')
-  return isValide
-}
-const handleFileUploaded = (rawData:IImageProps) => {
-  if (rawData._id) imageId = rawData._id
-}
 
 watch(() => error.value.status, () => {
   const { msg, status } = error.value
